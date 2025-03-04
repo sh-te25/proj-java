4721c622024732e9c21ec842264149dcd50fc29d
sh-te25 <tesemil90@gmail.com>
2025-03-04 18:14:39 +0000

    Update PorteEmbarquement.java

--- GestionAeroport/src/PorteEmbarquement.java
+++ GestionAeroport/src/PorteEmbarquement.java
@@ -2,4 +2,10 @@ public class PorteEmbarquement {
     Integer noPorteEmbarquement;
     Integer noTerminal;
     Boolean statut; // ouverte, fermée; deux options possibles donc j'ai mis Boolean.
+
+    public PorteEmbarquement(Integer npe, Integer nt, Boolean st) {
+        this.noPorteEmbarquement = npe;
+        this.noTerminal = nt;
+        this.statut = st;
+    }
 }
