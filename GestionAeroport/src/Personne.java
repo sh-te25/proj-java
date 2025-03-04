92bb9f191d57744add39ca08aa8e75b8562bb192
sh-te25 <tesemil90@gmail.com>
2025-03-04 18:13:22 +0000

    Update Personne.java

--- GestionAeroport/src/Personne.java
+++ GestionAeroport/src/Personne.java
@@ -1,4 +1,9 @@
 public class Personne {
     String nom;
     Integer numerodeContact;
+
+    public Personne(String n, Integer nc) {
+        this.nom = n;
+        this.numerodeContact = nc;
+    }
 }
