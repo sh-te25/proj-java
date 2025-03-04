bffe854ea8b5d2208c214d39bdc9fbcd0be0d8f1
sh-te25 <tesemil90@gmail.com>
2025-03-04 18:12:25 +0000

    Update Avion.java

--- GestionAeroport/src/Avion.java
+++ GestionAeroport/src/Avion.java
@@ -2,4 +2,10 @@ public class Avion {
     String constructeur;
     Integer capacite;
     String statut; // réparation, en ravitaillement, prête
+
+    public Avion(String cst, Integer cap, String st) {
+        this.constructeur = cst;
+        this.capacite = cap;
+        this.statut = st;
+    }
 }
